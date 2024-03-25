c89c0988cdf70d40ae79494cd8f5a9a4b8f81214
Azeez Adebayo <debaycisse@gmail.com>
2024-03-25 16:00:33 +0100

    add the README file
    add README file and other files

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - it creates a binary tree node
+ * 
+ * @parent: the parent node for the new node, about to be created
+ * @value: the integer value for the new node
+ *
+ * Return: pointer to the just created node or NULL, if failed
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Binary Tree
+
+This repo is used to house all tasks, related to the concept of binary tree in C
