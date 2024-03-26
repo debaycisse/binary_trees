8f31e6c60c057685fbd4e1ca659215aaa64a3f03
Azeez Adebayo <debaycisse@gmail.com>
2024-03-26 20:44:56 +0100

    implement task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,11 +8,11 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t number_of_edges = 0;
-
 	if (!tree)
 		return (0);
-
+	else if (tree->left)
+		return (binary_tree_height(tree->left) + 1);
+	else if (tree->right)
+		return (binary_tree_height(tree->right) + 1);
+	return (0);
 }
-
-
