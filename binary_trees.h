64519100d447b49f622ae19f209acccb7a4c5e94
Azeez Adebayo <debaycisse@gmail.com>
2024-03-26 14:41:16 +0100

    implement task 4

--- binary_trees.h
+++ binary_trees.h
@@ -37,6 +37,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 /* 3-binary_tree_delete.c */
 void binary_tree_delete(binary_tree_t *tree);
 
+/* 4-binary_tree_is_leaf.c */
+int binary_tree_is_leaf(const binary_tree_t *node);
+
 /* binary_tree_print.c */
 void binary_tree_print(const binary_tree_t *tree);
 void print_tree(binary_tree_t *root);
