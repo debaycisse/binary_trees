b89a929adb2ef22fac88ffee80ae6fb064c6a308
Azeez Adebayo <debaycisse@gmail.com>
2024-03-27 06:41:20 +0100

    work in progress

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,11 +8,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 		return (0);
 	binary_tree_size(tree->left);
 	binary_tree_size(tree->right);
-	if (tree)
-	{
-		if (tree->left)
-			return (1 + binary_tree_size(tree->left));
-		return (1 + binary_tree_size(tree->right));
-	}
-	return (0);
+	if (!tree)
+		return (0);
+	return (binary_tree_size(tree) + 1);
 }
