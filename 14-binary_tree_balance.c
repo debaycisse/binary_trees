b2c2bae4f8b0a733bc70746ef5b93d258ba80a51
Azeez Adebayo <debaycisse@gmail.com>
2024-03-29 12:07:57 +0100

    implement task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -4,10 +4,17 @@
  * binary_tree_balance - measures the balance factor of a binary tree
  *
  * @tree: pointer to the root node of the tree to measure the balance factor
- * Description: 
+ * Description: obtains the difference between left and right node of
+ * the given root
  * Return: the balance factor
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left = 0, right = 0;
 
+	if (!tree)
+		return (0);
+	left = tree->left ? binary_tree_balance(tree->left) + 1 : 0;
+	right = tree->right ? binary_tree_balance(tree->right) + 1 : 0;
+	return (left - right);
 }
